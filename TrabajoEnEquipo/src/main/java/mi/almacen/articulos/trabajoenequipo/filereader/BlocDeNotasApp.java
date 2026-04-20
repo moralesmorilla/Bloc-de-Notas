28c8aaea337ecd5437ff1b36befa629e0add44e0
moralesmorilla <moralesmorillamanuel@gmail.com>
2026-04-20 10:39:58 +0200

    nuevo

--- TrabajoEnEquipo/src/main/java/mi/almacen/articulos/trabajoenequipo/filereader/BlocDeNotasApp.java
+++ TrabajoEnEquipo/src/main/java/mi/almacen/articulos/trabajoenequipo/filereader/BlocDeNotasApp.java
@@ -11,15 +11,9 @@ package mi.almacen.articulos.trabajoenequipo.filereader;
 
 public class BlocDeNotasApp {
     public static void main(String[] args) {
-<<<<<<< HEAD:TrabajoEnEquipo/src/main/java/mi/almacen/articulos/trabajoenequipo/filereader/BlocDeNotasApp.java
         BlocDeNotasVista vista = new BlocDeNotasVista();
         BlocDeNotasControlador controlador = new BlocDeNotasControlador(vista);
         vista.setControlador(controlador);
-=======
-        
-        Controlador c = new Controlador();
-        c.iniciar();
-        
->>>>>>> f5a50062b65eab050099bfdbfc188e140e4ec21e:TrabajoEnEquipo/src/main/java/mi/almacen/articulos/trabajoenequipo/BlocDeNotas.java
+
     }
 }
